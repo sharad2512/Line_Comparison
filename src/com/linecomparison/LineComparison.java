622dfa00f4cdafef7b6e51dea13537bfd2be78c2
sharad2512 <shnarute95@gmail.com>
2023-03-13 18:54:08 +0530

    ADDED : Code to find length of line

--- src/com/linecomparison/LineComparison.java
+++ src/com/linecomparison/LineComparison.java
@@ -1,6 +1,22 @@
 package com.linecomparison;
+
+import java.util.Scanner;
+
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Programing");
+        Scanner scan = new Scanner(System.in);
+        float x1, y1, x2, y2, length1;
+
+        System.out.println(" Enter point for x1 and y1");
+        x1 = scan.nextFloat();
+        y1 = scan.nextFloat();
+
+        System.out.println(" Enter points for x2 and y2 ");
+        x2 = scan.nextFloat();
+        y2 = scan.nextFloat();
+
+        length1 = (float) Math.sqrt((Math.pow((x2 - x1), 2)) + (Math.pow((y2 - y1), 2)));
+        System.out.println("Length of line one is " + length1);
     }
 }
