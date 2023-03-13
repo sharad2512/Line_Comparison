005d5a89628d6e036da5016221c25c71709f5b3a
sharad2512 <shnarute95@gmail.com>
2023-03-13 18:51:06 +0530

    ADDED : Welcome Message to Line comparison programing

---
+++ src/com/linecomparison/LineComparison.java
@@ -0,0 +1,6 @@
+package com.linecomparison;
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Programing");
+    }
+}
